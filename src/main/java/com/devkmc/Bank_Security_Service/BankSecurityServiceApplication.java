4e099193d01ea57c7758937827335000fb046802
allcasa <allcasa@naver.com>
2024-11-01 13:23:07 +0900

    BankSystem

---
+++ src/main/java/com/devkmc/Bank_Security_Service/BankSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devkmc.Bank_Security_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSecurityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devkmc/Bank_Security_Service/BankSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkmc.Bank_Security_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
